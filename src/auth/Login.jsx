905c8feef4461594e139a50dc5ff47f59bad9eac
monilprajapati <monillprajapatii@gmail.com>
2024-08-27 23:06:23 +0530

    made some changes onlogin page

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -7,7 +7,10 @@ function Login() {
     const {GoogleLogin} = useContext(Contextpage);
 
     return (
-        <div className="h-screen flex justify-center items-center">
+        <div className="h-screen flex flex-col gap-10 justify-center items-center">
+            <div>
+                <h1 className='text-4xl text-white font-semibold'>Welcome to cineverse</h1>
+            </div>
             <div className='border-2 border-white/30 p-5 flex justify-center items-center gap-5 rounded-2xl cursor-pointer hover:bg-black' onClick={GoogleLogin}>
                 <FcGoogle className='text-3xl' />
                 <h1 className='text-white font-semibold'>Sign in with Google</h1>
